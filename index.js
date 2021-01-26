d079d342159bc7f1c2dea4e6b33c3a99bcedaf56
SMakaiTakori <makait87@icloud.com>
2021-01-25 22:56:59 -0500

    set up function to listen for port

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const portNumber = 4000;
 
 const app = express();
 console.log("App created...Attempting to listen on port:", portNumber);
+
+// ROUTES //
+
+// Listen for open port
+app.listen(portNumber, () => {
+  console.log("App running on port:", portNumber);
+});
