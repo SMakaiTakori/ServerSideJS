e4e38ee8cc7d323e97c050071207cdf2d771f9cd
SMakaiTakori <makait87@icloud.com>
2021-01-25 22:53:03 -0500

    required express and set up porNumber

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("Starting web app...");
+
+const express = require("express");
+const portNumber = 4000;
+
+const app = express();
+console.log("App created...Attempting to listen on port:", portNumber);
